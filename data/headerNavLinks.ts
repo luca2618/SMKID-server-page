137cdebcc0abfbc1ae2e2b14932d74be82dfc059
Lucas Sylvester <lucassylvester02@gmail.com>
2025-11-14 22:37:04 -0500

    Guidelines

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,6 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/kontakt', title: 'Kontakt' },
+  { href: '/guidelines', title: 'Guidelines' },
   { href: '/about', title: 'About' },
   { href: '/faq', title: 'FAQ' },
 ]
