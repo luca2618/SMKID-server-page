241e463e3c0010dbdb6f310d0cec19edb0554737
Lucas Sylvester <lucassylvester02@gmail.com>
2025-11-14 22:23:56 -0500

    Normal page generation

--- app/Main.tsx
+++ app/Main.tsx
@@ -3,16 +3,25 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 import NewsletterForm from 'pliny/ui/NewsletterForm'
+import { Pages, allPages } from 'contentlayer/generated'
+import { coreContent } from 'pliny/utils/contentlayer'
+import { MDXLayoutRenderer } from 'pliny/mdx-components'
 
 const MAX_DISPLAY = 5
 
 export default function Home({ posts }) {
+  const page = allPages.find((p) => p.slug === 'home') as Pages
+  const mainContent = coreContent(page)
+
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
-            Latest
+          <article className="prose dark:prose-invert max-w-none py-8">
+            <MDXLayoutRenderer code={page.body.code} />
+          </article>
+          <h1 className="text-xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-xl sm:leading-10 md:text-xl md:leading-14 dark:text-gray-100">
+            News
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -147,9 +147,23 @@ export const Authors = defineDocumentType(() => ({
   computedFields,
 }))
 
+export const Pages = defineDocumentType(() => ({
+  name: 'Pages',
+  filePathPattern: 'pages/**/*.mdx',
+  contentType: 'mdx',
+  fields: {
+    title: { type: 'string', required: false },
+    description: { type: 'string', required: false },
+    date: { type: 'date', required: false },
+    draft: { type: 'boolean', required: false },
+    layout: { type: 'string', required: false },
+  },
+  computedFields,
+}))
+
 export default makeSource({
   contentDirPath: 'data',
-  documentTypes: [Blog, Authors],
+  documentTypes: [Blog, Authors, Pages],
   mdx: {
     cwd: process.cwd(),
     remarkPlugins: [
